be3bef951723367c0f62bd8c428e88452d11e546
Siddhant-12081996 <siddhantbobde7425@gmail.com>
2022-12-30 04:20:48 +0530

    tip calci added

---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className="footer">
+        <p> Sid calculator 2022</p>
+     
+        </div>
+    )
+}
+
+export default Footer;
